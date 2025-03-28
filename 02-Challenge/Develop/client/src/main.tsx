0a375f9b934b4317d5f6c25f164b701d036adf54
delta_black5 <matt@kmgks.com>
2025-03-27 21:59:38 -0500

    commit

--- 02-Challenge/Develop/client/src/main.tsx
+++ 02-Challenge/Develop/client/src/main.tsx
@@ -4,5 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <>
+    <App />
+    <div>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+      <button className="btn btn-primary">Click Me!</button>
+      <p>Enjoy coding!</p>
+      <p>Happy coding!</p>
+      <p>Have a great day!</p>
+      <p>Keep learning!</p>
+      <p>Stay positive!</p>
+    </div>
+  </>
 )
